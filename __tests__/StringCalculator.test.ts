8dc41de40386ff171eabc5f97947605347cbb347
Srinivas <sririshi222@gmail.com>
2025-01-18 15:14:02 +0530

    Testing for a Single Number

--- __tests__/StringCalculator.test.ts
+++ __tests__/StringCalculator.test.ts
@@ -10,4 +10,7 @@ describe("StringCalculator", () => {
   it("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+  it("should return the number itself for a single number", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
 });
