27b8fb281f8c4042acf35a2330f7cc36e7ea5f11
Srinivas <sririshi222@gmail.com>
2025-01-18 15:05:05 +0530

    jest setup and StringCalculator test creation

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
 };
